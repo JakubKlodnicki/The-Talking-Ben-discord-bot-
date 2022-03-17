5518ee3e788e797479b75bf7cfcc899b09a927ff
Jakub Kłodnicki <96412859+JakubKlodnicki@users.noreply.github.com>
2022-03-17 13:52:00 +0100

    Update main,oy

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 import discord
 import random
 
-ben = ["Yes", "No", "Ohohoohho", "eeeeeh"]
+ben = ["Yes https://cdn.discordapp.com/attachments/887033566565912636/953998144268562472/yes.mp4", "No https://cdn.discordapp.com/attachments/887033566565912636/953998145317109770/no.mp4", "Ohohoohho https://cdn.discordapp.com/attachments/887033566565912636/953998144901877790/ohohoho.mp4", "Meeeeeh https://cdn.discordapp.com/attachments/887033566565912636/953998145673637929/meeeh.mp4"]
 
 client = discord.Client()
 
@@ -21,14 +21,14 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$ben'):
+    if message.content.startswith('$ben '):
         await message.channel.send(random.choice(ben))
 
     if message.content.startswith('$author'):
         await message.channel.send('My author is giga chad Jacob')
 
     if message.content.startswith('$help'):
-            await message.channel.send('`$ben (question) - Give question to ben`')
-            await message.channel.send('`$author - Bot author`') 
+        await message.channel.send('`$ben (question) - Give question to ben`')
+        await message.channel.send('`$author - Bot author`') 
 
-client.run('Token here')
+client.run('Token Here')
